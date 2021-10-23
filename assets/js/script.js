16353f3202319a5f743da0767df574efc4e80be9
Erin M <erin.michon@gmail.com>
2021-10-22 22:46:59 -0400

    added prompts without validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
-  //1. prompt the user for the password criteria
-  //  a. password length between 8-128 char
+   //  a. password length between 8-128 char
   //      user input will be a number
   //  b. lowercase
   //      user input will be a confirmation (yes/no to include)
@@ -23,6 +22,32 @@
 // Get references to the #generate element (button)
 let generateBtn = document.querySelector("#generate");
 
+//function for passwordcritera
+
+function userPasswordCriteria() {
+
+  //1. prompt the user for the password criteria  
+  let length = prompt("How many characters for your desired password?");
+  console.log(length);
+  //2. validate the input valdiate answer (password length between 8-128 char)
+
+  let includeUppercase = confirm("Would you like to include upper case characters in your password?")
+  console.log(includeUppercase);
+
+  let includeLowercase = confirm("Would you like to include lower case characters in your password?")
+  console.log(includeLowercase);
+
+  let includeNumeric = confirm("Would you like to include numeric characters in your password?")
+  console.log(includeNumeric);
+
+  let includeSpecial = confirm("Would you like to include special characters in your password?")
+  console.log(includeSpecial);
+  //2. validate the input - valdiate answers (must select at least one type of character)
+
+   
+
+};
+
 
 function generatePassword() {
   console.log("Hey you clicked a button!")
@@ -44,4 +69,6 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", userPasswordCriteria);
+
+//functions called
